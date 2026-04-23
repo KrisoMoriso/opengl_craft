c345f7a10dcdfb28c2f1693939ab172f93bb1110
Kris J <104223005+KrisoMoriso@users.noreply.github.com>
2026-04-23 15:45:25 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// ImGui headers
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+int main(){
+
+}
